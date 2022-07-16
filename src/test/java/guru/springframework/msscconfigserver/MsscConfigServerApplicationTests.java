6cf9db35105907dc856fdd4f81d65efdf07cc43a
zy51927042 <zy51927042@163.com>
2022-07-16 22:19:46 +0800

    init project add beer-service config

---
+++ src/test/java/guru/springframework/msscconfigserver/MsscConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.msscconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
